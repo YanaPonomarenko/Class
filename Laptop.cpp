5064d4e95ffbc4cdf31443433048ebd507d9909c
Yana Ponomarenko <y.ponomar05@gmail.com>
2025-10-29 20:57:05 +0200

    add class practice

---
+++ Laptop.cpp
@@ -0,0 +1,65 @@
+﻿#include "Laptop.h"
+
+Laptop::Laptop()
+{
+    model = "";
+    diagonal = 0.0;
+    manufacturer = "";
+    weight = 0.0;
+}
+
+Laptop::Laptop(string md, float d, string m, float w)
+{
+    model = md;
+    diagonal = d;
+    manufacturer = m;
+    weight = w;
+}
+
+string Laptop::getModel() const
+{
+    return model;
+}
+
+float Laptop::getDiagonal() const
+{
+    return diagonal;
+}
+
+string Laptop::getManufacturer() const
+{
+    return manufacturer;
+}
+
+float Laptop::getWeight() const
+{
+    return weight;
+}
+
+void Laptop::setModel(const string& md)
+{
+    model = md;
+}
+
+void Laptop::setDiagonal(float d)
+{
+    diagonal = d;
+}
+
+void Laptop::setManufacturer(const string& m)
+{
+    manufacturer = m;
+}
+
+void Laptop::setWeight(float w)
+{
+    weight = w;
+}
+
+ostream& operator<<(ostream& os, const Laptop& laptop)
+{
+    cout << "brand: " << laptop.model;
+    cout << "diagonal: " << laptop.diagonal;
+    cout << "manufacturer: " << laptop.manufacturer;
+    cout << "weight: " << laptop.weight;
+}
---
+++ Laptop.h
@@ -0,0 +1,36 @@
+﻿#pragma once
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Laptop
+{
+private:
+    string model;
+    float diagonal;
+    string manufacturer;
+    float weight;
+
+public:
+    
+    Laptop();
+
+    
+    Laptop(string md, float d, string m, float w);
+
+    
+    string getModel() const;
+    float getDiagonal() const;
+    string getManufacturer() const;
+    float getWeight() const;
+
+    void setModel(const string& b);
+    void setDiagonal(float d);
+    void setManufacturer(const string& m);
+    void setWeight(float w);
+
+    friend ostream& operator<<(ostream& os, const Laptop& laptop);
+};
+
+
