504c0ed4e418a152637524eee4807ac7ddb2e047
Yana Ponomarenko <y.ponomar05@gmail.com>
2025-11-03 20:30:29 +0200

    added practice

--- room_furniture.cpp
+++ room_furniture.cpp
@@ -11,7 +11,7 @@ public:
     string getName() const { return name; }
     void show() const
     {
-        cout << "Furniture: " << name << endl;
+        cout << "Furniture " << name << endl;
     }
 };
 
@@ -25,7 +25,7 @@ public:
     string getColor() const { return color; }
     void show() const
     {
-        cout << "Wall color: " << color << endl;
+        cout << "Wall color " << color << endl;
     }
 };
 
@@ -37,46 +37,28 @@ private:
     Furniture* furniture;
 public:
     Room() : name{ "NoName" }, furniture{ nullptr } {}
-    Room(string n, Wall w) : name{ n }, wall{ w }, furniture{ nullptr } {}
-
-    void setFurniture(Furniture* f) 
-    {
-        furniture = f;
-    }
-
+    Room(string n, Wall w, Furniture* f) : name{ n }, wall{ w }, furniture{ f } {}
     void show() const
     {
         cout << "Room: " << name << endl;
         wall.show();
         if (furniture)
-            cout << "Has furniture: " << furniture->getName() << endl;
+            cout << "Has furniture  " << furniture->getName() << endl;
         else
-            cout << "No furniture." << endl;
+            cout << "No furniture" << endl;
     }
 };
 
 int main()
 {
-    Wall wall1("White");
-    Room room1("Living Room", wall1);
-
     Furniture furniture1("Table");
-    room1.setFurniture(&furniture1);
-    room1.show();
+    Room* room1 = new Room("Living Room", Wall("Pink"), &furniture1);
+    room1->show();
+    delete room1;
 
-    cout << "\n--- Demonstrating aggregation ---" << endl;
-
-    Furniture furniture2("Chair");
-
-    Wall wall2("Blue");
-    Room room2("Bedroom", wall2);
-    room2.setFurniture(&furniture2);
-    room2.show();
-
-    Wall wall3("Green");
-    Room room3("Kitchen", wall3);
-    room3.setFurniture(&furniture2);
-    room3.show();
+    Room* room2 = new Room("Kitchen", Wall("Green"), &furniture1);
+    room2->show();
+    delete room2;
 
     return 0;
 }
