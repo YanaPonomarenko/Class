b7005d3b1da609743d7dfeef93571a0366159fba
Yana Ponomarenko <y.ponomar05@gmail.com>
2025-11-14 16:30:33 +0200

    added practice 12.11

---
+++ SmartLamp.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+class Light
+{
+public:
+    void on() { cout << "Light is on\n"; }
+    void off() { cout << "Light is off\n"; }
+};
+
+class WifiDevice : virtual public Light
+{
+public:
+    void connect() { cout << "wifi device connected\n"; }
+};
+
+class SmartLamp : public WifiDevice
+{
+public:
+    void voiceControl() { cout << "Voice control activated\n"; }
+};
+
+int main()
+{
+    SmartLamp lamp;
+
+    lamp.on();
+    lamp.off();
+    lamp.connect();
+    lamp.voiceControl();
+
+    return 0;
+}
